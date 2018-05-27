b07249ce4e702ca00955ae7b060cee731a475f43
jyoticice <39135407+jyoticice@users.noreply.github.com>
2018-05-27 06:36:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.walmart.com/ip/iLive-IAHB6B-Bluetooth-Wireless-Headphones-with-Microphone-Black/54503362?athcpid=54503362&athpgid=athenaItemPage&athcgid=null&athznid=PWVAV&athieid=v0&athstid=CS002&athguid=466001f5-689e120d-40809ecf73d5c06d&athena=true");
 die();
 ?>
